4a79b654dd12d776db571e7a79db7a461ae08bce
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-13 21:04:23 +0530

    Test Drive of user profile in View

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init Github
 const github = new Github;
 
+// Init UI
+const ui = new UI;
+
 // Search Input
 const searchUser = document.getElementById('searchUser');
 
@@ -13,7 +16,16 @@ searchUser.addEventListener('keyup', (e) => {
     // Make http call
     github.getUser(userText)
     .then(data => { 
-      console.log(data);
+      if(data.profile.message === 'Not Found') {
+        // Show Alert
+
+      } else {
+        // Show Profile
+        ui.showProfile(data.profile);
+      }
     })
-  } 
+  } else {
+    // Clear Profile
+
+  }
 });
